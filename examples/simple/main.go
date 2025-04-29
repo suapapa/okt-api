7a64deb26170ca09bdc6cf54a4b243c7a6945076
Homin Lee <ff4500@gmail.com>
2025-04-30 05:24:22 +0900

    add more in example

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"regexp"
+	"strings"
 
+	"github.com/goccy/go-yaml"
 	"github.com/suapapa/okt-go/pkg/okt"
 )
 
@@ -14,15 +18,73 @@ func main() {
 		log.Fatal(err)
 	}
 
-	normalized, err := okt.NormalizeText("안녕하세요")
+	meta, body, err := parseHugoPage("sample.md")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(">>", normalized)
 
-	phrases, err := okt.ExtractPhrases("키스의 조건은 눈을 감아야 한다.")
+	normalized, err := okt.NormalizeText(body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(">>", phrases)
+	fmt.Println("normalized : ", len(normalized))
+
+	chunks := simpleKoreanSplitter(normalized)
+	fmt.Println("chunks : ", len(chunks))
+
+	phrases, err := okt.ExtractPhrases(normalized)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("phrases : ", strings.Join(phrases, ","), len(phrases))
+
+	fmt.Println("meta : ", meta)
+}
+
+func parseHugoPage(fp string) (map[string]interface{}, string, error) {
+	data, err := os.ReadFile(fp)
+	if err != nil {
+		panic(err)
+	}
+	content := string(data)
+
+	// --- 으로 구분
+	parts := strings.SplitN(content, "---", 3)
+	if len(parts) < 3 {
+		return nil, "", fmt.Errorf("Front Matter를 찾을 수 없습니다.")
+	}
+
+	yamlPart := parts[1]
+	markdownPart := strings.TrimSpace(parts[2])
+
+	// YAML 파싱
+	var frontMatter map[string]interface{}
+	err = yaml.Unmarshal([]byte(yamlPart), &frontMatter)
+	if err != nil {
+		return nil, "", fmt.Errorf("YAML 파싱 오류: %w", err)
+	}
+
+	// 결과 출력
+	// fmt.Println("Front Matter (map):")
+	// fmt.Println(frontMatter)
+
+	// fmt.Println("\nMarkdown 본문 (string):")
+	// fmt.Println(markdownPart)
+
+	return frontMatter, markdownPart, nil
+}
+
+func simpleKoreanSplitter(text string) []string {
+	// 마침표, 느낌표, 물음표, "다 ", "요 " 기준으로 나눈다
+	re := regexp.MustCompile(`([^\n.!?다요]+[다요.!?])`)
+	matches := re.FindAllString(text, -1)
+
+	var chunks []string
+	for _, match := range matches {
+		clean := strings.TrimSpace(match)
+		if len(clean) > 0 {
+			chunks = append(chunks, clean)
+		}
+	}
+	return chunks
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/suapapa/okt-go
 
 go 1.24.2
+
+require github.com/goccy/go-yaml v1.17.1
